7d57bdff0eb9e3909b1dca5e42c247bc84c82da6
John Morgan <johnmorgan302@gmail.com>
2018-10-05 13:30:24 -0500

    Reformatted to make script easier to read.

--- LockDown.ps1
+++ LockDown.ps1
@@ -1,65 +1,91 @@
-# KSC Threat Response Script copy write John Morgan 2017.
-# This script is distributed using GPLv3 License.
-# https://www.gnu.org/licenses/gpl-3.0.en.html
+# ====[ KSC LockDown Script for use with ProofPoint Threat Response ]===== 
+# Copyright John Morgan 2017,2018.
 # This script comes with no warranty either expressed or implied.
-# This script is intended to be used/deployed by ProofPoint Threat Response
-# and reports data back to PPTR to be used in incident response.  This script
-# can be run directly from the PowerShell console if you wish.
+#
+# This script is distributed using GPLv3 License 
+# https://www.gnu.org/licenses/gpl-3.0.en.html
+# In order to get documentation and the lastest 
+# version of the script, referenc the GIT repository 
+# https://github.com/johnmorgan302/LockDown 
 
-Write-Output "Beginning Script..."
-# Action taken is determined by the KSC naming scheme.  You will need to modify the script to
-# take advantage of your organization's naming scheme, or key off of some other computer attribute.
 
-#Attempt to delete any old tasks if they exist.
-schtasks /delete /tn reboot /f
+#=[ VARIABLES ]===========================================================
+
+# REBOOT TIME - Don't reboot the workstation until it has had time to 
+#               report data back to Threat Response.
+$rebootDelay = 10 #Minutes
+$rebootStatus = "Reboot Delay = " + $rebootDelay + " minutes"
+Write-Output $rebootStatus 
+$rebootTime = (get-date).AddMinutes($rebootDelay).ToString("HH:mm")
+
+# UNLOCK CODE - We need a code complex enough that users can't guess it,
+#               but simple enough that they can type it.  We use the
+#               minute and second the event script started running
+#               formatted as a four digit number.
+#               $a = Get-Date -Format mmss
+#
+#               If you want a static code, you can simple set $a to
+#               a value of your choosing.
+#               $a = "1234"
+$a = Get-Date -Format mmss
+#              WARNING: The script seeks to write a file with this 
+#              code into c:\temp.  If c:\temp doesn't exist on your 
+#              machines, uncomment the line below.
+#mkdir c:\temp
+
+
+#=[ HOUSE KEEPING ]======================================================
+
+# Report back to the console that things are getting under way.
+Write-Output "Beginning Script."
+
+# Attempt to delete any old tasks if they exist.
+# Our tasks are prepended with KSC to make them easier to
+# search for - Example: schtasks /query | findstr -i ksc
+Write-Output "Cleaning old tasks (failure means no tasks to clean)."
+schtasks /delete /tn kscreboot /f
 schtasks /delete /tn ksclockdown /f	
 
-#Grab all errors from the event log for the last 30 minutes.
-Write-Output "Gathering Log Data..."
-Write-Output "Application Log"
-Get-EventLog application -After (Get-Date).AddMinutes(-30) | Where-Object{$_.EntryType -ne 'Information' } | Format-Table -autosize -wrap -Property TimeGenerated,Message
-Write-Output "System Log"
-Get-EventLog system -After (Get-Date).AddMinutes(-30) | Where-Object{$_.EntryType -ne 'Information' } | Format-Table -autosize -wrap -Property TimeGenerated,Message
-Write-Output "Preparing Response"
 
-#Figure out when to reboot
-$then = (get-date).AddMinutes(10).ToString("HH:mm")
+#=[ HANDLE DIFFERENT SYSTEM TYPES]=======================================
+# Our non-persistent VDI systems all begin with "VDI".  
+# Reboot the system to return it to a known good state.
+# I know we are calling this a reboot, but it's actually
+# a shutdown.
 
-# Our non-persistent VDI systems all begin with "VDI".  Reboot the system to return it to a known good state.
 if( $env:ComputerName -like "VDI*"){
 	Write-Output "VDI no unlock code generated - System Schedule for Reboot"
 	#Schedule a shutdown
-	schtasks /create /tn "reboot" /tr "shutdown -s -f -d P:5:19 -t 60" /ru system /sc once /st $then	
-	# Old shutdown method was causing a race condition.
-	#	&shutdown -r -f -c "DO NOT LOGOFF, SHUTDOWN, or REBOOT!`r`n`r`nThe system will automatically reboot in 5 minutes.`r`n`r`nSAVE YOUR WORK!`r`n`r`nKelsey-Seybold Information Security is investigating this system for malware.  You will be notified if any further action is necessary.`r`n`r`n" -d P:5:19 -t 300
+	schtasks /create /tn "kscreboot" /tr "shutdown -s -f -d P:5:19 -t 60" /ru system /sc once /st $rebootTime	
 # VM2 is the name given to all persistent VDI.  Shut these down.	
 }elseif( $env:ComputerName -like "VM2*" ){
 	Write-Output "Persistent no unlock code generated - System Shut Down"
 	#Schedule a shutdown
-	schtasks /create /tn "reboot" /tr "shutdown -s -f -d P:5:19 -t 60" /ru system /sc once /st $then
-	# Old shutdown method was causing a race condition.	
-	#	&shutdown -s -f -c "DO NOT LOGOFF, SHUTDOWN, or REBOOT!`r`n`r`nSAVE YOUR WORK!`r`n`r`nThe system will automatically shut down in 5 minutes.`r`n`r`nDO NOT restart without speaking to Information Security`r`n`r`nKelsey-Seybold Information Security is investigating this system for malware.  We will be attempting to call you shortly.  If you haven't heard from us within 10 minutes, call the Infosec On-Call Phone: 713-364-4636." -d P:5:19 -t 300
-# KS1 and VM1 are servers.  do not impede their ability to work.	
+	schtasks /create /tn "kscreboot" /tr "shutdown -s -f -d P:5:19 -t 60" /ru system /sc once /st $rebootTime
+# KS1 and VM1 are servers.  TAKE NO ACTION!
 }elseif( $env:ComputerName -like "VM1*" -or $env:ComputerName -like "KS1*" ){
-	Write-Output "Server!  No action taken"
+	# Our analysts should be sharp enough to know this,
+	# but spelling it out never hurts.
+	Write-Output "Server!  No action taken!"
 }else{
-	# All other systems are physical workstations.  We will warn the
-	# user, then place a pair of lock and unlock scripts on the device
-	# and shut it down.
+	# All other system names are physical workstations.  We will warn the
+	# We will schedule a reboot job (shutdown), and schedule a second
+	# job to disable all network interfaces when the system restarts.
 	
-	# Generate the unlock code.
-	# The four digit code is the minute and second the script ran.
-	# This is not intended to be cryptographically sound, just
-	# complex enough that the users can't memorize/share the code.
-	$a = Get-Date -Format mmss
-	$b = 'myCode=' + $a
+	# The key code is determined in the variables section at the top of
+	# the script and is called $a.
+
 	# Write the code to a text file named "key.txt"
 	write-output $a > 'c:\temp\key.txt'
-	# Send the unlock code to the Threat Response Console.
-	Write-Output "Unlock Code: $a"
-	#Create a script to lock and unlock the network ports on the system.
-	#The first write creates the file.
-	write-output "Create Lockdown Script..."
+	# NOTE: If the line above failes, go back to the
+	# VARIABLES section and uncomment the line to
+	# Make the c:\temp directory.
+	
+	# Create a script to lock and unlock the network ports on the system.
+	# The first write creates the file.  Subsequent lines are appended.
+	# If the script is truncated, make sure that someone hasn't
+	# accidentally replaced >> with >.
+	write-output "Create Lockdown Script."
 	write-output 'strComputer = "."' > 'c:\temp\lockdown.vbs'
 	#All subsequent writes use the append >> operator.
 	write-output 'Set obj = CreateObject("Scripting.FileSystemObject")' >> 'c:\temp\lockdown.vbs'
@@ -89,7 +115,11 @@ if( $env:ComputerName -like "VDI*"){
 	write-output 'obj.DeleteFile("C:\programdata\microsoft\windows\start menu\programs\startup\KSCUnlock.vbs")' >> 'c:\temp\lockdown.vbs'
 	
 	# Write a VBScript to allow the user to see a dialog box to unlock the computer.
-	Write-Output "Writing Unlock Script..."
+	# In order to be seen the file must be in the STARTUP folder of the Program
+	# Files menu. (C:\programdata\microsoft\windows\start menu\programs\startup\KSCUnlock.vbs)
+	Write-Output "Writing Unlock Script."
+	$myString =  "Unlock Code = " + $a
+	Write-Output $myString
 	$b='strCode="' + $a + '"'
 	write-output $b > 'C:\programdata\microsoft\windows\start menu\programs\startup\KSCUnlock.vbs'
 	write-output 'while NOT(strInput = strCode)' >> 'C:\programdata\microsoft\windows\start menu\programs\startup\KSCUnlock.vbs'
@@ -103,9 +133,17 @@ if( $env:ComputerName -like "VDI*"){
 	# http://www.leeholmes.com/blog/2008/07/30/workaround-the-os-handles-position-is-not-what-filestream-expected/
 	schtasks /create /tn "KSCLockDown" /tr "c:\Windows\SysWOW64\cscript.exe c:\temp\lockdown.vbs" /ru system /sc ONSTART
 	#Schedule a shutdown
-	schtasks /create /tn "reboot" /tr "shutdown -s -f -d P:5:19 -t 60" /ru system /sc once /st $then
-	# Old shutdown method was causing a race condition.
-	#&shutdown -s -f -c "DO NOT LOGOFF, SHUTDOWN, or REBOOT!`r`n`r`nThe system will automatically shut down in 5 minutes.`r`n`r`nSAVE YOUR WORK!`r`n`r`nKelsey-Seybold Information Security is investigating this system for malware.  We will be attempting to call you shortly.  If you haven't heard from us within 10 minutes, call the Information Security On-Call Number: 713-364-4636." -d P:5:19 -t 300
+	schtasks /create /tn "kscreboot" /tr "shutdown -s -f -d P:5:19 -t 60" /ru system /sc once /st $rebootTime
 }
-Write-Output "Script Complete..."
+
+# Grab all errors from the Application and System Event logs for the last 30 minutes.
+# And output them to the ProofPoint Threat Response console.
+Write-Output "Gathering Log Data (Only severity greater than Information will be shown)."
+Write-Output "Application Log"
+Get-EventLog application -After (Get-Date).AddMinutes(-30) | Where-Object{$_.EntryType -ne 'Information' } | Format-Table -autosize -wrap -Property TimeGenerated,Message
+Write-Output "System Log"
+Get-EventLog system -After (Get-Date).AddMinutes(-30) | Where-Object{$_.EntryType -ne 'Information' } | Format-Table -autosize -wrap -Property TimeGenerated,Message
+
+# Nofity the Analyst that the script has completed.
+Write-Output "Script Complete."
 exit 0
